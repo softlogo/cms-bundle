1a6f4851eac78e1d908022e59b5cee14c27e4800
Borys <borysjank@gmail.com>
2015-05-11 17:39:54 +0200

    Mailing

---
+++ Tools/MailHelper.php
@@ -0,0 +1,59 @@
+<?php
+namespace Softlogo\CMSBundle\Tools;
+
+class MailHelper
+{
+	protected $mailer;
+	protected $twig;
+	
+	public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
+	{
+		$this->mailer = $mailer;
+		$this->twig = $twig;
+	}
+
+	public function sendEmail($from, $to, $subject, $body)
+	{
+		$message = \Swift_Message::newInstance()
+			->setContentType('text/html')
+			->setSubject($subject)
+			->setFrom($from)
+			->setTo($to)
+			->setBody($body)
+		;
+		$this->mailer->send($message);
+	}
+
+	public function sendEmailWithView($from, $to, $subject, $view, $parameters)
+	{
+		$body = $this->twig->render($view, $parameters);
+		$message = \Swift_Message::newInstance()
+			->setContentType('text/html')
+			->setSubject($subject)
+			->setFrom($from)
+			->setTo($to)
+			->setBody($body)
+		;
+		$this->mailer->send($message);
+	}
+
+	public function sendEmailWithAttachments($from, $to, $subject, $body, Array $paths)
+	{
+		$message = \Swift_Message::newInstance()
+			->setContentType('text/html')
+			->setSubject($subject)
+			->setFrom($from)
+			->setTo($to)
+			->setBody($body)
+		;
+		
+		foreach($paths as $path)
+		{
+			$attachment = \Swift_Attachment::fromPath(realpath($path));
+			$message->attach($attachment);
+		}
+		
+		$this->mailer->send($message);
+	}
+}
+?>
